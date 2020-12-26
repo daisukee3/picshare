5225b8683cb6c80bf602bd1207eb62d9154f9338
daisuke_nabatame <nabanaba1225@gmail.com>
2020-12-26 15:10:49 +0900

    articleのnewの詳細pageの作成、flashの導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect to articles_path(@article), notice: '保存完了'
+    else
+      flash.now[:error] = '保存失敗'
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
