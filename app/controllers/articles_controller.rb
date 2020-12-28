f9b10bbf545e5aad5a52679df6948421ec09fdbf
daisuke_nabatame <nabanaba1225@gmail.com>
2020-12-28 18:21:36 +0900

    before_actionを定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update]
+
   def index
     @articles = Article.all
   end
@@ -18,11 +20,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params:[:id])
     if @article.update(article_params)
       redirect to articles_path(@article), notice: '更新完了'
     else
@@ -40,5 +40,9 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+      @article = Article.find(params[:id])
   end
 end
