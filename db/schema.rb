4b063f0961aa2580b0aa02b624017fc3c74d7f78
daisuke_nabatame <nabanaba1225@gmail.com>
2021-01-04 17:22:47 +0900

    DBのスキーマを修正（タイトルを削除）

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_12_22_073002) do
 
   create_table "articles", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
