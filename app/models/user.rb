d17241f17a98a17f9dd88fdb447ffb9e4d2a9671
daisuke_nabatame <nabanaba1225@gmail.com>
2021-01-06 18:44:32 +0900

    フォローの関係を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,9 +25,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
+
   has_many :articles,dependent: :destroy
   has_many :likes,dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
 
   def has_written?(article)
     articles.exists?(id: article.id)
@@ -35,6 +40,10 @@ class User < ApplicationRecord
 
   def has_liked?(article)
     likes.exists?(article_id: article.id)
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
   end
 
   def prepare_profile
