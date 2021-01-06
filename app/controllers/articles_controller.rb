d39ed99be1d1edd80d20e6a3c5e80c4556796946
daisuke_nabatame <nabanaba1225@gmail.com>
2021-01-06 13:36:29 +0900

    画像を複数投稿できるように

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content, images: [])
   end
 
 end
