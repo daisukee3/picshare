79172812a54df8a7faf0a2eb57975b092b833adb
daisuke_nabatame <nabanaba1225@gmail.com>
2021-01-17 18:24:49 +0900

    いいねの状態をjQueryで反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -41,7 +41,10 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.inactive-heart').on('click', () => {
       axios.post(`/articles/${articleId}/like`)
       .then((response) => {
-        console.log(response)
+        if (response.data.status === 'ok') {
+          $('.active-heart').removeClass('hidden')
+          $('.inactive-heart').addClass('hidden')
+        }
       })
       .catch((e) => {
         window.alert('Error')
@@ -52,7 +55,10 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.active-heart').on('click', () => {
       axios.delete(`/articles/${articleId}/like`)
       .then((response) => {
-        console.log(response)
+        if (response.data.status === 'ok') {
+          $('.active-heart').addClass('hidden')
+          $('.inactive-heart').removeClass('hidden')
+        }
       })
       .catch((e) => {
         window.alert('Error')
