2f5c379cc3d1f890f99747cd511128763091522f
daisuke_nabatame <nabanaba1225@gmail.com>
2021-01-17 16:31:16 +0900

    可読性のためハートの表示をコントロールするfunctionを定義。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  } else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+}
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -25,10 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
   axios.get(`/articles/${articleId}/like`)
     .then((response) =>  {
       const hasLiked = response.data.hasLiked
-      if (hasLiked) {
-        $('.active-heart').removeClass('hidden')
-      } else {
-        $('.inactive-heart').removeClass('hidden')
-      }
+      handleHeartDisplay(hasLiked)
     })
 })
