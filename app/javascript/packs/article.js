344569fffff6912a7af5f060697bb4977e119425
daisuke_nabatame <nabanaba1225@gmail.com>
2021-01-18 12:10:42 +0900

    ajaxで各記事のidを取得し個別でいいねできるよう調整

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -6,48 +6,86 @@ axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 //いいね機能
 
-const handleHeartDisplay = (hasLiked) => {
-  if (hasLiked) {
-    $('.active-heart').removeClass('hidden')
-  } else {
-    $('.inactive-heart').removeClass('hidden')
-  }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  const dataset = $('#card-show').data()
-  const articleId = dataset.articleId
-  axios.get(`/articles/${articleId}/like`)
-    .then((response) =>  {
-      const hasLiked = response.data.hasLiked
-      handleHeartDisplay(hasLiked)
-    })
 
-    $('.inactive-heart').on('click', () => {
+  $(function(){
+    $(`.inactive-heart`).on('click', function() {
+      const articleId = $(this).attr('id')
       axios.post(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'ok') {
-          $('.active-heart').removeClass('hidden')
-          $('.inactive-heart').addClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
+        .then((response) => {
+          if (response.data.status === 'ok') {
+            $(this).addClass('hidden')
+            $(`#${articleId}.active-heart`).removeClass('hidden')
+          }
+        })
+        .catch((e) => {
+          window.alert('Error')
+          console.log(e)
+        })
     })
+  })
 
-    $('.active-heart').on('click', () => {
+  $(function(){
+    $(`.active-heart`).on('click', function() {
+      const articleId = $(this).attr('id')
       axios.delete(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === 'ok') {
-          $('.active-heart').addClass('hidden')
-          $('.inactive-heart').removeClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
+        .then((response) => {
+          if (response.data.status === 'ok') {
+            $(`#${articleId}.inactive-heart`).removeClass('hidden')
+            $(this).addClass('hidden')
+          }
+        })
+        .catch((e) => {
+          window.alert('Error')
+          console.log(e)
+        })
     })
+  })
 })
+// const handleHeartDisplay = (hasLiked) => {
+//   if (hasLiked) {
+//     $('.active-heart').removeClass('hidden')
+//   } else {
+//     $('.inactive-heart').removeClass('hidden')
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const dataset = $('#card-show[id]').data()
+//   const articleId = dataset.articleId
+//   axios.get(`/articles/${articleId}/like`)
+//     .then((response) =>  {
+//       const hasLiked = response.data.hasLiked
+//       handleHeartDisplay(hasLiked)
+//     })
+
+//     $('.inactive-heart').on('click', () => {
+//       const likeId = $(this).attr('id')
+//       axios.post(`/articles/${likeId}/like`)
+//       .then((response) => {
+//         if (response.data.status === 'ok') {
+//           $(`#${likeId}.active-heart`).removeClass('hidden')
+//           $(`#${likeId}.inactive-heart`).addClass('hidden')
+//         }
+//       })
+//       .catch((e) => {
+//         window.alert('Error')
+//         console.log(e)
+//       })
+//     })
+
+//     $('.active-heart').on('click', () => {
+//       const likeId = $(this).attr('id')
+//       axios.delete(`/articles/${likeId}/like`)
+//       .then((response) => {
+//         if (response.data.status === 'ok') {
+//           $(`#${likeId}.active-heart`).addClass('hidden')
+//           $(`#${likeId}.inactive-heart`).removeClass('hidden')
+//         }
+//       })
+//       .catch((e) => {
+//         window.alert('Error')
+//         console.log(e)
+//       })
+//     })
+// })
