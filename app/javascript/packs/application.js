a525aac49b63a59812b88defd2cfc1b53138f782
daisuke_nabatame <nabanaba1225@gmail.com>
2021-01-25 18:16:08 +0900

    一部リファクタリング

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,18 +19,30 @@ import $ from 'jquery'
 import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 
-document.addEventListener('DOMContentLoaded', () => {
-  
-  // コメント機能
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
+const handleCommentForm = () => {
   $('.show-comment-form').on('click', () => {
     $('.show-comment-form').addClass('hidden')
     $('.comment-text-area').removeClass('hidden')
   })
+}
+
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class="article_comment"><p>${comment.content}</p></div>`
+  )
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  handleCommentForm()
+
+  // コメント機能
 
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
-  
+
   $('.add-comment-button').on('click', () => {
     const content = $('#comment_content').val()
     if (!content) {
@@ -41,9 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
       })
         .then((res) => {
           const comment = res.data
-          $('.comments-container').append(
-            `<div class="article_comment"><p>${comment.content}</p></div>`
-          )
+          appendNewComment(comment)
           $('#comment_content').val('')
         })
     }
@@ -53,9 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
   .then((response) => {
     const comments = response.data
     comments.forEach((comment) => {
-      $('.comments-container').append(
-        `<div class="article_comment"><p>${comment.content}</p></div>`
-      )
+      appendNewComment(comment)
     })
   }) 
 
